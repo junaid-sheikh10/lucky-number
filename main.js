62403291310a2a3e2271bb05c08fc7290331aa13
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-29 18:30:27 +0530

    final commit

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
 var input=document.querySelector("#input");
 var lucky=document.querySelector("#lucky");
 var button=document.querySelector("#btn");
+var output=document.querySelector("#output-div")
 
-button.addEventListener(click,checkinput);
+button.addEventListener("click",checkinput);
 
-function checkinput(){
-    console.log("clicked")
-    var inp =input.value;
-    var luc=lucky.value;
-    if(inp && luc){
+function datesum(date){
+    let sum=0;
+    date=date.replaceAll("-","");
+    for(let d of date){
+        sum=sum+Number(d);
+    }
+    return sum
+}
 
+function calculateluckynum (date,num){
+    
+    if(date%num===0){
+        console.log("lucky num")
+        var lo=num+ " is a lucky number";
+        output.innerHTML=lo;
     }
     else{
-        showMessage("please enter both fields")
-        console.log("else")
+        console.log("not lucky enough");
+        var numo="not lucky enough";
+        output.innerHTML=numo;
+
     }
+}
+
+function checkinput(){
+    console.log("clicked");
+    var inp =input.value;
+    var luc=lucky.value;
+    if (inp && luc) {
+      
+    var dsum= datesum(inp);
+    calculateluckynum(dsum,luc);
+        
+    }
+
+    else{
+        console.log("enter both fileds");
+        var m="enter both fields"
+        output.innerHTML=m
+
+    }
+    
 }
